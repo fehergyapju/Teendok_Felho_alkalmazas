0b95ccb63e3cf82d216d57da52bb64990b0a0956
Gyapjú Fehér <mate.szabolcska@gmail.com>
2025-06-01 19:29:30 +0200

    Működik a nem megcsinált teendők megjelenítése

---
+++ TodoList/Pages/NemMegcsinalt.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace TodoList.Pages
+{
+    public class NemMegcsinaltModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
