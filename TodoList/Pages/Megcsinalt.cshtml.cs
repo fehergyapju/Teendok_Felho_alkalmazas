1d8b9e6aaf0f65bb40b741ae1f0a80d804275890
Gyapjú Fehér <mate.szabolcska@gmail.com>
2025-06-11 18:41:52 +0200

    Most már látjuk a megcsinált teendőket.

---
+++ TodoList/Pages/Megcsinalt.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace TodoList.Pages
+{
+    public class MegcsinalaktModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
